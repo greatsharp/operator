e976f9686dd4b8085102b8eb3474c9255a12c3b7
Raphael <greatsharp@hotmail.com>
2022-08-14 21:35:34 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+	pkg "operator/pkg"
+)
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		inClusterConfig, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatalln("can not get config")
+		}
+		config = inClusterConfig
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalln("can not get client")
+	}
+
+	factory := informers.NewSharedInformerFactory(clientset, 0)
+	serviceInformer := factory.Core().V1().Services()
+	ingressInformer := factory.Networking().V1().Ingresses()
+
+	controller := pkg.NewController(clientset, serviceInformer, ingressInformer)
+	stopCh := make(chan struct{})
+	factory.Start(stopCh)
+	factory.WaitForCacheSync(stopCh)
+	fmt.Println("controller run...")
+	controller.Run(stopCh)
+}
